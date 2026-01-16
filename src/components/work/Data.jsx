1fb509c9a973ba57387d0e54526ef95270ae7db4
Wasifpatel <wasifpatel007@gmail.com>
2026-01-16 23:35:01 +0530

    solved deployment error 2

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,8 +1,6 @@
 import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.png";
 import Work3 from "../../assets/work3.png";
-import Work4 from "../../assets/work4.png";
-import Work5 from "../../assets/work5.png";
 import Work6 from "../../assets/Work6.png";
 import Work7 from "../../assets/work7.png";
 export const projectsData = [
